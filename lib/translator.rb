29e558e7788be3579185b6bd900035733c24bcf0
Jordan Dains <dains.jordan2@gmail.com>
2020-09-21 05:01:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,19 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(file_location)
+  emotes = YAML.load_file(file_location)
+  hash = emotes.each_with_object ({}) do |(key, value), new_hash| 
+    new_hash[key] = {:english => value[0], :japanese => value[1]}
+  end
+  #binding.pry
+  return hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_location, emote)
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_location, emote)
+  
 end
