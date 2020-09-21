08b120030c4a7f3726b87da0a374527d7b7256eb
Jordan Dains <dains.jordan2@gmail.com>
2020-09-21 05:09:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,16 @@ def load_library(file_location)
 end
 
 def get_japanese_emoticon(file_location, emote)
-  
+  emotes = load_library(file_location)
+  emotes.each do 
+    |name, language_hash|
+    if language_hash[:english] == emote then
+      return language_hash[:japanese]
+    end
+  end
+  return "Sorry, that emoticon was not found"
+end
+
 end
 
 def get_english_meaning(file_location, emote)
